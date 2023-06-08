3be81972dea03412a05760f2d2fbbf92df9d90dc
Alex Weitzel <alexanderjohnweitzel@gmail.com>
2023-06-08 16:42:59 -0700

    Scripts to run commands from within the phast docker image. just run ./run_phast.sh from /work/ and follow it with a command in "command"

---
+++ inside_docker/phast_scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#empy, just for test
+say_a_word() {
+    local print_me=$1
+    echo $print_me
+}
---
+++ outside_docker/run_phast.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#./scripts/outside_docker/split_count_concat_maf.sh -s subset -i ./scripts/inside_docker/ -o ./chr5_test_mafs/ -c ./output/peak_maf_size.txt -t ./output/concat.maf
+
+main () {
+    local command=$1
+
+
+    #start_docker_image 
+    local phast_container='my_phast'
+    local script_source='./scripts/maf_splitter/inside_docker/phast_scripts.sh'
+    source ./scripts/maf_splitter/outside_docker/docker_utils.sh #contains start_docker_image and d_wrap
+
+    start_docker_image $phast_container "e3d0affb583b" 
+
+    d_wrap $phast_container $script_source "$command"
+
+
+    docker stop $phast_container
+    docker rm $phast_container
+}
+
+main "$@"
